466309e4add3b9641777428c59c6c71fb39b9ff7
Виталий <vitaliy.anatolyevich@gmail.com>
2022-07-28 19:26:47 +0300

    added on ViewModel fun getImages

--- app/src/main/java/com/testtask/testnatife/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/testtask/testnatife/presentation/viewmodels/MainViewModel.kt
@@ -1,7 +1,35 @@
 package com.testtask.testnatife.presentation.viewmodels
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.testtask.testnatife.core.viewmodels.BaseViewModel
+import com.testtask.testnatife.domain.models.ImageModel
+import com.testtask.testnatife.domain.usecases.GetImages
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor(
+    private val getImagesUseCase: GetImages
+): BaseViewModel() {
+
+    private val _imagesData = MutableLiveData<List<ImageModel>>()
+    val imagesData: LiveData<List<ImageModel>>
+        get() = _imagesData
+
+    fun getImages(query: String) {
+        getImagesUseCase(
+            GetImages.Params(
+               query = query,
+                limit = 20,
+                offset = 0,
+                rating = "g",
+                lang = "uk"
+            )
+        ) {
+            it.either(::handleFailure, ::handleImages)
+        }
+    }
+
+    private fun handleImages(images: List<ImageModel>) {
+        _imagesData.value = images
+    }
 }
