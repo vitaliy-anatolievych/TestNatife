5a9cb15adb66a3e851013a38cc9a625cc739bca3
Виталий <vitaliy.anatolyevich@gmail.com>
2022-07-28 19:39:56 +0300

    make fragment container and fun launchFragment

--- app/src/main/java/com/testtask/testnatife/presentation/MainActivity.kt
+++ app/src/main/java/com/testtask/testnatife/presentation/MainActivity.kt
@@ -2,13 +2,29 @@ package com.testtask.testnatife.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.testtask.testnatife.R
 import com.testtask.testnatife.di.DaggerAppComponent
+import com.testtask.testnatife.presentation.contracts.Navigator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goToMainScreen()
+    }
+
+    override fun goToMainScreen() {
+
+    }
+
+    private fun launchFragment(fragment: Fragment, isAddToBackStack: Boolean) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_fragment_container, fragment, fragment::class.simpleName)
+            if (isAddToBackStack) addToBackStack(fragment::class.simpleName)
+            commit()
+        }
     }
 }
