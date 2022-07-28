085addc780fd2494769953d7f9f7b5caa9c8aba1
Виталий <vitaliy.anatolyevich@gmail.com>
2022-07-28 12:38:14 +0300

    delete koin

--- app/src/main/java/com/testtask/testnatife/di/AppModule.kt
+++
@@ -1,6 +0,0 @@
-package com.testtask.testnatife.di
-
-import org.koin.dsl.module
-
-
-val appModule = module {  }
--- app/src/main/java/com/testtask/testnatife/di/DomainModule.kt
+++
@@ -1,5 +0,0 @@
-package com.testtask.testnatife.di
-
-import org.koin.dsl.module
-
-val domainModule = module {  }
